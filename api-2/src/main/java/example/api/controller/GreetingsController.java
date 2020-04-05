8770632af51fca3643b2c4d2e78fec8939a454c2
Milvik <neha.doddamani@milvik.se>
2020-04-05 05:46:36 +0530

    initial commit

---
+++ api-2/src/main/java/example/api/controller/GreetingsController.java
@@ -0,0 +1,24 @@
+package example.api.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import example.api.util.Greeting;
+
+@RestController
+public class GreetingsController {
+
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/greeting")
+  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    return new Greeting(counter.incrementAndGet(),
+              String.format(template, name));
+  }
+
+}
